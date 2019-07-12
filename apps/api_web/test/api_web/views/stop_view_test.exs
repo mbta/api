f5d3774da7517e99a9fb2d41827dc277eae0e74c
Paul Handorff <handorff.paul@gmail.com>
2019-07-12 09:08:45 -0400

    Add basic StopView test

--- apps/api_web/test/api_web/views/stop_view_test.exs
+++ apps/api_web/test/api_web/views/stop_view_test.exs
@@ -2,8 +2,43 @@ defmodule ApiWeb.StopViewTest do
   @moduledoc false
   use ApiWeb.ConnCase, async: true
   import Phoenix.View
+  import ApiWeb.StopView
   alias ApiWeb.StopView
   alias Model.Stop
+
+  @stop %Stop{
+    id: "72",
+    name: "Massachusetts Ave @ Pearl St",
+    description: "description",
+    latitude: 42.364915,
+    longitude: -71.103074,
+    municipality: "Cambridge",
+    on_street: "Massachusetts Avenue",
+    at_street: "Essex Street",
+    vehicle_type: 3
+  }
+
+  test "can do a basic rendering", %{conn: conn} do
+    rendered = render("index.json-api", data: @stop, conn: conn)["data"]
+    assert rendered["type"] == "stop"
+    assert rendered["id"] == @stop.id
+
+    assert rendered["attributes"] == %{
+             "name" => @stop.name,
+             "description" => @stop.description,
+             "latitude" => @stop.latitude,
+             "longitude" => @stop.longitude,
+             "municipality" => @stop.municipality,
+             "on_street" => @stop.on_street,
+             "at_street" => @stop.at_street,
+             "vehicle_type" => @stop.vehicle_type,
+             "address" => nil,
+             "location_type" => 0,
+             "platform_code" => nil,
+             "platform_name" => nil,
+             "wheelchair_boarding" => 0
+           }
+  end
 
   test "encodes the self link to be URL safe", %{conn: conn} do
     id = "River Works / GE Employees Only"
