4479e8d845c2bf46bec475de826309b3e62b1a88
jfabian <jfabian@mbta.com>
2019-08-22 16:29:13 -0400

    Satisfy formatting

--- apps/state/config/config.exs
+++ apps/state/config/config.exs
@@ -120,23 +120,23 @@ config :state, :stops_on_route,
       ["place-FB-0148", "place-FB-0166", "place-FB-0177", "place-FB-0191"]
     ],
     {"Green-B", 0} => [
-      ["place-north", "place-haecl", "place-gover"],
+      ["place-north", "place-haecl", "place-gover"]
     ],
     {"Green-B", 1} => [
-      ["place-gover", "place-haecl", "place-north"],
+      ["place-gover", "place-haecl", "place-north"]
     ],
     {"Green-C", 0} => [
-      ["place-north", "place-haecl", "place-gover"],
+      ["place-north", "place-haecl", "place-gover"]
     ],
     {"Green-C", 1} => [
-      ["place-gover", "place-haecl", "place-north"],
+      ["place-gover", "place-haecl", "place-north"]
     ],
     {"Green-E", 0} => [
-      ["place-north", "place-haecl", "place-gover"],
+      ["place-north", "place-haecl", "place-gover"]
     ],
     {"Green-E", 1} => [
-      ["place-gover", "place-haecl", "place-north"],
-    ],
+      ["place-gover", "place-haecl", "place-north"]
+    ]
   }
 
 import_config "#{Mix.env()}.exs"
