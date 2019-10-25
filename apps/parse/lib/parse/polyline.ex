2dfdb39b5cce205c1e0d05ffa48dbf3bad23d248
Paul Swartz <paul@paulswartz.net>
2019-10-25 14:40:04 -0400

    perf: use SimpleCSV for parsing shapes
    There isn't anything to quote.

--- apps/parse/lib/parse/polyline.ex
+++ apps/parse/lib/parse/polyline.ex
@@ -9,7 +9,8 @@ defmodule Parse.Polyline do
 
   def parse(blob) when is_binary(blob) do
     blob
-    |> ExCsv.parse!(headings: true)
+    |> BinaryLineSplit.stream!()
+    |> SimpleCSV.stream()
     |> Enum.group_by(& &1["shape_id"])
     |> Enum.map(&parse_shape(elem(&1, 1)))
   end
@@ -23,14 +24,10 @@ defmodule Parse.Polyline do
 
   defp polyline(points) do
     points
-    |> Enum.sort_by(&Integer.parse(&1["shape_pt_sequence"]))
-    |> Enum.flat_map(fn %{"shape_pt_lat" => lat, "shape_pt_lon" => lon} ->
-      with {lon, ""} <- Float.parse(lon),
-           {lat, ""} <- Float.parse(lat) do
-        [{lon, lat}]
-      else
-        _ -> []
-      end
+    |> Enum.map(fn %{"shape_pt_lat" => lat, "shape_pt_lon" => lon} ->
+      {lon, ""} = Float.parse(lon)
+      {lat, ""} = Float.parse(lat)
+      {lon, lat}
     end)
     |> Polyline.encode()
   end
