f99807456cec9d81d8155021296ec87efd97d259
Paul Handorff <handorff.paul@gmail.com>
2019-12-11 12:40:06 -0500

    Add rating start/end dates, description to API (#164)

--- apps/parse/test/parse/calendar_attributes_test.exs
+++ apps/parse/test/parse/calendar_attributes_test.exs
@@ -5,8 +5,8 @@ defmodule Parse.CalendarAttributesTest do
 
   test "parses calendar entries" do
     blob = """
-    service_id,service_description,service_schedule_name,service_schedule_type,service_schedule_typicality
-    BUS12019-hba19ns1-Weekday-02,Weekday schedule (no school),Weekday (no school),Weekday,1
+    service_id,service_description,service_schedule_name,service_schedule_type,service_schedule_typicality,rating_start_date,rating_end_date,rating_description
+    BUS12019-hba19ns1-Weekday-02,Weekday schedule (no school),Weekday (no school),Weekday,1,20181222,20190314,Winter
     """
 
     assert CalendarAttributes.parse(blob) == [
@@ -15,15 +15,18 @@ defmodule Parse.CalendarAttributesTest do
                description: "Weekday schedule (no school)",
                schedule_name: "Weekday (no school)",
                schedule_type: "Weekday",
-               schedule_typicality: 1
+               schedule_typicality: 1,
+               rating_start_date: ~D[2018-12-22],
+               rating_end_date: ~D[2019-03-14],
+               rating_description: "Winter"
              }
            ]
   end
 
   test "parses calendar entries with missing fields" do
     blob = """
-    service_id,service_description,service_schedule_name,service_schedule_type,service_schedule_typicality
-    BUS12019-hba19ns1-Weekday-02,,,,
+    service_id,service_description,service_schedule_name,service_schedule_type,service_schedule_typicality,rating_start_date,rating_end_date,rating_description
+    BUS12019-hba19ns1-Weekday-02,,,,,,,
     """
 
     assert CalendarAttributes.parse(blob) == [
@@ -32,7 +35,10 @@ defmodule Parse.CalendarAttributesTest do
                description: nil,
                schedule_name: nil,
                schedule_type: nil,
-               schedule_typicality: nil
+               schedule_typicality: nil,
+               rating_start_date: nil,
+               rating_end_date: nil,
+               rating_description: nil
              }
            ]
   end
