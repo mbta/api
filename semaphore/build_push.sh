da06dfce347462defd04ced85a682f1df100a1a9
Paul Swartz <paul@paulswartz.net>
2020-02-04 07:50:29 -0500

    feat: build Docker container and push to ECS

---
+++ semaphore/build_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e -x -u
+
+# other required configuration:
+# * APP
+# * DOCKER_REPO
+
+# build docker image and tag it with git hash and aws environment
+githash=$(git rev-parse --short HEAD)
+docker build -t $APP:latest .
+docker tag $APP:latest $DOCKER_REPO:git-$githash
+
+# push images to ECS image repo
+docker push $DOCKER_REPO:git-$githash
