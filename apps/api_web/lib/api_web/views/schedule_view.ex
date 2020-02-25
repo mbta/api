7d02ca29a04681beff9603239baf9d7e81a83ff8
Paul Swartz <paul@paulswartz.net>
2020-02-25 15:41:12 -0500

    perf: speed up ScheduleView
    Rather than iterating over the map, create a regular map and then do the
    special handling for the two special cases of arrival_time and departure_time.

--- apps/api_web/lib/api_web/views/schedule_view.ex
+++ apps/api_web/lib/api_web/views/schedule_view.ex
@@ -30,28 +30,38 @@ defmodule ApiWeb.ScheduleView do
   ])
 
   def attributes(schedule, conn) do
-    base = %{
-      arrival_time: fn -> arrival_time(schedule, conn) end,
-      departure_time: fn -> departure_time(schedule, conn) end,
-      stop_sequence: schedule.stop_sequence,
-      pickup_type: schedule.pickup_type,
-      drop_off_type: schedule.drop_off_type,
-      timepoint: schedule.timepoint?,
-      direction_id: schedule.direction_id
-    }
-
     opts = Map.get(conn.assigns, :opts, [])
 
-    attributes =
+    fields =
       case Keyword.get(opts, :fields) do
         %{"schedule" => fields} ->
-          Map.take(base, fields)
+          fields
+
+        _ ->
+          Map.keys(inlined_attributes_map(schedule, conn))
+      end
+
+    base =
+      schedule
+      |> Map.put(:timepoint, schedule.timepoint?)
+      |> Map.take(fields)
+
+    base =
+      case base do
+        %{arrival_time: time} ->
+          Map.put(base, :arrival_time, format_time(time, conn))
 
         _ ->
           base
       end
 
-    Map.new(attributes, &apply_function_values/1)
+    case base do
+      %{departure_time: time} ->
+        Map.put(base, :departure_time, format_time(time, conn))
+
+      _ ->
+        base
+    end
   end
 
   def id(%{trip_id: trip_id, stop_id: stop_id, stop_sequence: stop_sequence}, _conn) do
@@ -83,14 +93,6 @@ defmodule ApiWeb.ScheduleView do
     State.Prediction.prediction_for(schedule, date)
   end
 
-  def arrival_time(%{arrival_time: seconds_past_midnight}, conn) do
-    format_time(seconds_past_midnight, conn)
-  end
-
-  def departure_time(%{departure_time: seconds_past_midnight}, conn) do
-    format_time(seconds_past_midnight, conn)
-  end
-
   defp format_time(nil, _) do
     nil
   end
@@ -103,13 +105,5 @@ defmodule ApiWeb.ScheduleView do
     end
     |> DateHelpers.add_seconds_to_date(seconds)
     |> DateTime.to_iso8601()
-  end
-
-  defp apply_function_values({key, value}) when is_function(value, 0) do
-    {key, value.()}
-  end
-
-  defp apply_function_values(key_value) do
-    key_value
   end
 end
