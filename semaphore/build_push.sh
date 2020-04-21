86a895e255244838583bca7734a456a274c852ac
Maldonado <emaldonado@mbta.com>
2020-04-21 09:17:37 -0400

    chore: bump erlang to 22.3.2

--- semaphore/build_push.sh
+++ semaphore/build_push.sh
@@ -7,7 +7,7 @@ set -e -x -u
 
 # build docker image and tag it with git hash and aws environment
 githash=$(git rev-parse --short HEAD)
-docker build -t $APP:latest .
+docker build --pull -t $APP:latest .
 docker tag $APP:latest $DOCKER_REPO:git-$githash
 
 # push images to ECS image repo
