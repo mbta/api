5ec591d4ab3582cd116ef31b35402c9013e219ce
jfabian <jfabian@mbta.com>
2020-11-09 17:30:51 -0500

    fix: No more Foxboro service

--- apps/state_mediator/test/state_mediator/integration/gtfs_test.exs
+++ apps/state_mediator/test/state_mediator/integration/gtfs_test.exs
@@ -212,61 +212,6 @@ defmodule StateMediator.Integration.GtfsTest do
 
       assert invalid_dates == []
     end
-
-    test "Foxboro is on CR-Franklin after 2019-10-21" do
-      foxboro? = fn stops ->
-        Enum.find(stops, &(&1.id == "place-FS-0049")) != nil
-      end
-
-      assert foxboro?.(State.Stop.filter_by(%{routes: ["CR-Franklin"]}))
-      assert foxboro?.(State.Stop.filter_by(%{routes: ["CR-Franklin"], direction_id: 0}))
-      assert foxboro?.(State.Stop.filter_by(%{routes: ["CR-Franklin"], direction_id: 1}))
-
-      invalid_dates =
-        for date <- dates_of_rating(),
-            direction_id <- [nil, 0, 1],
-            data =
-              State.Stop.filter_by(%{
-                routes: ["CR-Franklin"],
-                direction_id: direction_id,
-                date: date
-              }),
-            after_oct21? = Date.compare(date, ~D[2019-10-21]) != :lt,
-            has_foxboro? = foxboro?.(data),
-            after_oct21? != has_foxboro? do
-          {date, direction_id}
-        end
-
-      assert invalid_dates == []
-    end
-
-    test "Foxboro and Dedham Corporate Center are on CR-Fairmount after 2019-10-21" do
-      valid? = fn stops ->
-        Enum.find(stops, &(&1.id == "place-FS-0049")) != nil and
-          Enum.find(stops, &(&1.id == "place-FB-0118")) != nil
-      end
-
-      assert valid?.(State.Stop.filter_by(%{routes: ["CR-Fairmount"]}))
-      assert valid?.(State.Stop.filter_by(%{routes: ["CR-Fairmount"], direction_id: 0}))
-      assert valid?.(State.Stop.filter_by(%{routes: ["CR-Fairmount"], direction_id: 1}))
-
-      invalid_dates =
-        for date <- dates_of_rating(),
-            direction_id <- [nil, 0, 1],
-            data =
-              State.Stop.filter_by(%{
-                routes: ["CR-Fairmount"],
-                direction_id: direction_id,
-                date: date
-              }),
-            after_oct21? = Date.compare(date, ~D[2019-10-21]) != :lt,
-            is_valid? = valid?.(data),
-            after_oct21? != is_valid? do
-          {date, direction_id}
-        end
-
-      assert invalid_dates == []
-    end
   end
 
   describe "shapes" do
