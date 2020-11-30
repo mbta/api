c39bc393aea572bfb81754b2ea1adf9dda9ce24a
fjlanasa@gmail.com <fjlanasa@gmail.com>
2020-11-30 17:01:22 -0500

    fixup! fix: deprioritize shape 811_0010 for Green-B

--- apps/state/config/config.exs
+++ apps/state/config/config.exs
@@ -37,9 +37,9 @@ config :state, :shape,
     "811_0007" => -1,
     # Green-B (North Station)
     "811_0008" => -1,
-    # Green-B (North Station),
+    # Green-B (North Station)
     "811_0009" => -1,
-    # Green-B (North Station),
+    # Green-B (North Station)
     "811_0010" => -1,
     # Green-B
     "813_0003" => 2,
