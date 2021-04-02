5863e82aec29f7b7fe5e13e39b2e0e39339df52d
Joshua Fabian <jfabian@mbta.com>
2021-04-02 17:15:42 -0400

    chore: Add Worcester Line connection shuttle to timetable (#358)

--- apps/state/config/config.exs
+++ apps/state/config/config.exs
@@ -24,7 +24,10 @@ config :state, :route_pattern,
     "Shuttle-AlewifeLittletonExpress-0-0" => false,
     "Shuttle-AlewifeLittletonExpress-0-1" => false,
     "Shuttle-AlewifeLittletonLocal-0-0" => false,
-    "Shuttle-AlewifeLittletonLocal-0-1" => false
+    "Shuttle-AlewifeLittletonLocal-0-1" => false,
+    # don't ignore Newton Connection RailBus for Worcester Line
+    "Shuttle-NewtonHighlandsWellesleyFarms-0-0" => false,
+    "Shuttle-NewtonHighlandsWellesleyFarms-0-1" => false
   }
 
 config :state, :shape,
@@ -274,6 +277,28 @@ config :state, :stops_on_route,
         "place-ER-0227",
         "place-ER-0208",
         "place-ER-0183"
+      ]
+    ],
+    {"CR-Worcester", 0} => [
+      [
+        "place-WML-0035",
+        "place-newtn",
+        "place-WML-0081",
+        "place-WML-0091",
+        "place-WML-0102",
+        "place-river",
+        "place-WML-0125"
+      ]
+    ],
+    {"CR-Worcester", 1} => [
+      [
+        "place-WML-0125",
+        "place-river",
+        "place-WML-0102",
+        "place-WML-0091",
+        "place-WML-0081",
+        "place-newtn",
+        "place-WML-0035"
       ]
     ]
   },
