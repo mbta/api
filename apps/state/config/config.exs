37534079d11b4965a64ff534437ed6585cd48345
Joshua Fabian <jfabian@mbta.com>
2022-03-14 19:33:03 -0400

    chore: Supress GLX shapes, stops from dotcom pre-opening

--- apps/state/config/config.exs
+++ apps/state/config/config.exs
@@ -153,7 +153,10 @@ config :state, :shape,
   },
   suffix_overrides: %{
     # shuttles are all -1 priority
-    "-S" => -1
+    "-S" => -1,
+    # Prior to GLX opening on March 21st, suppress Union Square shapes
+    "8000001" => -1,
+    "8000002" => -1
   }
 
 # Overrides whether specific trips (by route pattern prefix) should be used in determining the
@@ -375,6 +378,9 @@ config :state, :stops_on_route,
       "place-spmnl"
     ],
     {"Green-E", 0} => [
+      "place-unsqu",
+      "place-lech",
+      "place-spmnl",
       "14159",
       "21458",
       "9070206",
@@ -382,6 +388,9 @@ config :state, :stops_on_route,
       "4510"
     ],
     {"Green-E", 1} => [
+      "place-unsqu",
+      "place-lech",
+      "place-spmnl",
       "14155",
       "21458",
       "4510",
