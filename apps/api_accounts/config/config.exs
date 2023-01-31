283533336d6199458e2d4d6d5f1131885cbe232c
Nick Stein <nstein@mbta.com>
2023-01-31 16:48:30 -0500

    feat: use jshmrtn/email_checker to validate emails on user create and update

--- apps/api_accounts/config/config.exs
+++ apps/api_accounts/config/config.exs
@@ -14,6 +14,12 @@ config :ex_aws, :hackney_opts,
   recv_timeout: 30_000,
   pool: :ex_aws_pool
 
+config :email_checker,
+  default_dns: {8, 8, 8, 8},
+  smtp_retries: 1,
+  timeout_milliseconds: 6000,
+  validations: [EmailChecker.Check.Format, EmailChecker.Check.MX]
+
 config :api_accounts, ApiAccounts.Mailer, adapter: Bamboo.SesAdapter
 
 config :api_accounts, migrate_on_start: false
