b1c138aea4b99a11f7ff3216cb6c952e6340f9d4
Nick Stein <nstein@mbta.com>
2023-03-02 15:51:58 -0500

    chore: fix credo suggestion

--- apps/api_web/lib/api_web/sentry_event_filter.ex
+++ apps/api_web/lib/api_web/sentry_event_filter.ex
@@ -1,4 +1,7 @@
 defmodule ApiWeb.SentryEventFilter do
+  @moduledoc """
+    Provides a filter for exceptions coming from 404 errors
+  """
   @behaviour Sentry.EventFilter
 
   def exclude_exception?(%Phoenix.Router.NoRouteError{}, :plug), do: true
