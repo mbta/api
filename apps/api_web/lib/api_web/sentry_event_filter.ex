0e0b4808ae222de2da4b77b766c09b57a8214ff9
Nick Stein <nstein@mbta.com>
2023-03-02 17:12:05 -0500

    wip: see if event is coming from another source

--- apps/api_web/lib/api_web/sentry_event_filter.ex
+++ apps/api_web/lib/api_web/sentry_event_filter.ex
@@ -5,5 +5,7 @@ defmodule ApiWeb.SentryEventFilter do
   @behaviour Sentry.EventFilter
 
   def exclude_exception?(%Phoenix.Router.NoRouteError{}, :plug), do: true
+  def exclude_exception?(%Elixir.Phoenix.Router.NoRouteError{}, :logger), do: true
+  def exclude_exception?(%Elixir.Phoenix.Router.NoRouteError{}, :endpoint), do: true
   def exclude_exception?(_exception, _source), do: false
 end
