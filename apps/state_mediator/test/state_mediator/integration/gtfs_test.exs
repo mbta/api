fe50fb83bf1805b3129ddc5944c967fc83c589ec
Brian Fauble <bfauble@users.noreply.github.com>
2023-05-12 13:47:42 -0500

    undo reenable of newburyport/rockport test

--- apps/state_mediator/test/state_mediator/integration/gtfs_test.exs
+++ apps/state_mediator/test/state_mediator/integration/gtfs_test.exs
@@ -205,15 +205,16 @@ defmodule StateMediator.Integration.GtfsTest do
                shapes_1
     end
 
-    test "Newburyport/Rockport has 2 non-ignored shapes each direction" do
-      [shapes_0, shapes_1] = shapes_in_both_directions("CR-Newburyport")
-
-      assert [%{name: "North Station - Newburyport"}, %{name: "North Station - Rockport"}] =
-               shapes_0
-
-      assert [%{name: "Newburyport - North Station"}, %{name: "Rockport - North Station"}] =
-               shapes_1
-    end
+    # Disable Newburyport/Rockport Line check while Rockport Branch is closed, Fall 2022
+    # test "Newburyport/Rockport has 2 non-ignored shapes each direction" do
+    #  [shapes_0, shapes_1] = shapes_in_both_directions("CR-Newburyport")
+    #
+    #  assert [%{name: "North Station - Rockport"}, %{name: "North Station - Newburyport"}] =
+    #           shapes_0
+    #
+    #  assert [%{name: "Rockport - North Station"}, %{name: "Newburyport - North Station"}] =
+    #           shapes_1
+    # end
 
     test "all shuttle shapes have negative priority" do
       invalid_shapes =
