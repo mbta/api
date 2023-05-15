084153ff84848adbbf6a630545ba802996c9ef0b
Brian Fauble <bfauble@users.noreply.github.com>
2023-05-15 14:14:13 -0500

    remove ibm parking info from state mediator and config (#613)

--- apps/state_mediator/config/config.exs
+++ apps/state_mediator/config/config.exs
@@ -30,8 +30,6 @@ config :state_mediator, State.Vehicle,
     "https://cdn.mbta.com/realtime/VehiclePositions_enhanced.json"
   }
 
-config :state_mediator, State.Facility.Parking, source: {:system, "IBM_PARKING_SOURCE", ""}
-
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
