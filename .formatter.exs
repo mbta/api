ebb6cdb73d1ca3d910a590cec2b53c5dc8a4842b
Paul Swartz <paul@paulswartz.net>
2023-10-03 19:02:49 -0400

    chore: reformat templates

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,7 @@
 # Used by "mix format"
 [
-  inputs: ["mix.exs", "apps/*/{config,lib,test}/**/*.{ex,exs}"],
-  rename_deprecated_at: "1.6.0"
+  inputs: ["mix.exs", "apps/*/{config,lib,test}/**/*.{heex,ex,exs}"],
+  import_deps: [:phoenix],
+  plugins: [Phoenix.LiveView.HTMLFormatter],
+  rename_deprecated_at: "1.14.3"
 ]
