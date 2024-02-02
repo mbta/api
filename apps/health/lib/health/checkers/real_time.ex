cba0035d958a4481eaed4683d80ed2de3446a5db
Brian Fauble <bfauble@users.noreply.github.com>
2024-02-02 10:36:06 -0700

    add health_check logs

--- apps/health/lib/health/checkers/real_time.ex
+++ apps/health/lib/health/checkers/real_time.ex
@@ -6,10 +6,16 @@ defmodule Health.Checkers.RealTime do
   @stale_data_seconds 15 * 60
   @current_time_fetcher &DateTime.utc_now/0
 
-  def current do
+  def current(current_time_fetcher \\ @current_time_fetcher) do
     updated_timestamps = State.Metadata.updated_timestamps()
+    current_time = current_time_fetcher.()
 
-    [prediction: updated_timestamps.prediction, vehicle: updated_timestamps.vehicle]
+    [
+      prediction: updated_timestamps.prediction,
+      prediction_diff: DateTime.diff(current_time, updated_timestamps.prediction),
+      vehicle: updated_timestamps.vehicle,
+      vehicle_diff: DateTime.diff(current_time, updated_timestamps.vehicle)
+    ]
   end
 
   def healthy?(current_time_fetcher \\ @current_time_fetcher) do
