943d97e171113bdf18bf1d01efed45e2a5c49748
cmaddox5 <christian.maddox93@gmail.com>
2024-12-19 10:59:43 -0500

    Add back a test.

--- apps/api_web/test/api_web/canary_test.exs
+++ apps/api_web/test/api_web/canary_test.exs
@@ -36,4 +36,13 @@ defmodule ApiWeb.CanaryTest do
 
     assert_receive {:EXIT, ^pid, :normal}
   end
+
+  test "stops with reason if given a value that is not a function for notify_fn" do
+    Process.flag(:trap_exit, true)
+
+    assert {:error, "expect function/0 for notify_fn, got nil"} =
+             Canary.start_link(notify_fn: nil)
+
+    assert_receive {:EXIT, _, "expect function/0 for notify_fn, got nil"}
+  end
 end
