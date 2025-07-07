2566c7433094150d4db2a51d961d38b8b3d5566b
Walton Hoops <me@waltonhoops.com>
2025-07-07 14:50:27 -0600

    chore: log max run_queue and don't fail health checks based on it

--- apps/health/test/health/checkers/run_queue_test.exs
+++ apps/health/test/health/checkers/run_queue_test.exs
@@ -2,51 +2,17 @@ defmodule Health.Checkers.RunQueueTest do
   use ExUnit.Case
   import Health.Checkers.RunQueue
 
-  describe "log_processes/2" do
-    test "logs if we're not healthy and the log level is low enough" do
-      assert log_processes(false, :info) == :logged
-      assert log_processes(false, :debug) == :logged
-    end
-
-    test "does nothing when we're healthy" do
-      assert log_processes(true, :info) == :ignored
-    end
-
-    test "does nothing when the log level is high" do
-      assert log_processes(false, :warning) == :ignored
+  describe "healthy?/0" do
+    test "always returns true" do
+      assert healthy?() == true
     end
   end
 
-  describe "log_lines/0" do
-    test "one line per alive process" do
-      lines = log_lines()
-      assert length(lines) >= length(Process.list())
-    end
-  end
-
-  describe "log_info/1" do
-    test "logs information about the process" do
-      binary = IO.iodata_to_binary(log_info(self()))
-      assert binary =~ ~s(current_function="Elixir.Process.info/2")
-      assert binary =~ ~s(initial_call="erlang.apply/2")
-      assert binary =~ ~s(message_queue_len=0)
-      assert binary =~ ~s(status=running)
-    end
-
-    test "overrides initial call if present in process dictionary" do
-      # GenServers set this
-      {:ok, pid} = Agent.start_link(fn -> :ok end)
-      binary = IO.iodata_to_binary(log_info(pid))
-
-      assert binary =~
-               ~s(initial_call="Elixir.Health.Checkers.RunQueueTest.-test log_info/1 overrides initial call if present in process dictionary/1-fun-0-/0")
-    end
-
-    test "logs a dead process" do
-      {:ok, pid} = Agent.start_link(fn -> :ok end)
-      Agent.stop(pid)
-      binary = IO.iodata_to_binary(log_info(pid))
-      assert binary =~ ~s(status=dead)
+  describe "current/0" do
+    test "returns the current run queue size" do
+      [run_queue: size] = current()
+      assert is_integer(size)
+      assert size >= 0
     end
   end
 end
