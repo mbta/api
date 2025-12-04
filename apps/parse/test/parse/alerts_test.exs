f0b54abc5f46714937bed3e6790b63729b640a33
Melody Horn <mhorn@mbta.com>
2025-12-04 11:09:59 -0700

    fix formatting

--- apps/parse/test/parse/alerts_test.exs
+++ apps/parse/test/parse/alerts_test.exs
@@ -818,7 +818,12 @@ defmodule Parse.AlertsTest do
 
       unix_epoch = iso_date("1970-01-01 00:00:00Z")
 
-      assert [%Alert{active_period: [{^unix_epoch, ^unix_epoch}], closed_timestamp: ^closed_timestamp}] =
+      assert [
+               %Alert{
+                 active_period: [{^unix_epoch, ^unix_epoch}],
+                 closed_timestamp: ^closed_timestamp
+               }
+             ] =
                parse_json(data)
     end
 
