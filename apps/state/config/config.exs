37867d2a7f430fb00536db9553fff5e35fbfe652
Eddie Maldonado <emaldonado@mbta.com>
2026-02-17 11:29:51 -0500

    fix: try removing unnecessary `place-sstat`

--- apps/state/config/config.exs
+++ apps/state/config/config.exs
@@ -300,8 +300,7 @@ config :state, :stops_on_route,
         "place-DB-2249",
         "place-DB-2258",
         "place-DB-2265",
-        "place-NEC-2203",
-        "place-sstat"
+        "place-NEC-2203"
       ]
     ],
     {"CR-Fairmount", 0} => [
