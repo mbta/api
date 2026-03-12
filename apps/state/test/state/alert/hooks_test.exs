d8aed98dd9c89a06146d96267bf90abb674f7bdf
Jon Zimbel <63608771+jzimbel-mbta@users.noreply.github.com>
2026-03-12 10:02:40 -0400

    fix: Only merge activities on computed parent station informed entities, not original child stop informed entities (#974)
    * test: Only merge activities on computed parent station informed entities, not original child stop i.e.s
    
    * fix: Only merge activities on computed parent station informed entities, not original child stop i.e.s
    
    * tweak(test): Check grouping logic
    
    * Clean up test module
    
    * fix: Address review comments

--- apps/state/test/state/alert/hooks_test.exs
+++ apps/state/test/state/alert/hooks_test.exs
@@ -1,34 +1,538 @@
 defmodule State.Alert.HooksTest do
   @moduledoc false
-  use ExUnit.Case, async: true
+  use ExUnit.Case
   import State.Alert.Hooks
+  alias Model.Alert
 
-  @alert %Model.Alert{
-    id: "alert1",
-    informed_entity: [
-      %{
-        route_type: 3,
-        route: "1",
-        direction_id: 0,
-        stop: "place-cool",
-        activities: ["BOARD", "RIDE"]
-      },
-      %{
-        route_type: 3,
-        route: "1",
-        direction_id: 0,
-        stop: "place-cool",
-        activities: ["EXIT"]
-      }
-    ],
-    severity: 1
-  }
+  setup do
+    State.Stop.new_state([])
+    State.Trip.new_state([])
+    State.Route.new_state([])
+    :ok
+  end
 
-  test "pre_insert_hook/1 merges informed entities" do
-    [alert] = pre_insert_hook(@alert)
-    ie = Map.get(alert, :informed_entity)
-    assert is_list(ie)
-    assert length(ie) == 1
-    assert length(ie |> hd |> Map.get(:activities)) == 3
+  @type hook_result :: %{
+          added: [Alert.informed_entity()],
+          removed: [Alert.informed_entity()],
+          preserved: [Alert.informed_entity()]
+        }
+
+  @spec apply_hook([Alert.informed_entity()]) :: hook_result
+  @spec apply_hook(Alert.t()) :: hook_result
+  defp apply_hook(informed_entities) when is_list(informed_entities) do
+    apply_hook(%Alert{id: "alert1", informed_entity: informed_entities})
+  end
+
+  defp apply_hook(%Alert{} = alert) do
+    assert [%Alert{informed_entity: new_informed_entities}] = pre_insert_hook(alert)
+
+    old = MapSet.new(alert.informed_entity)
+    new = MapSet.new(new_informed_entities)
+
+    [
+      added: MapSet.difference(new, old),
+      removed: MapSet.difference(old, new),
+      preserved: MapSet.intersection(old, new)
+    ]
+    |> Map.new(fn {k, ies} -> {k, normalize(ies)} end)
+  end
+
+  defp normalize(ies) do
+    ies
+    |> Enum.map(fn ie -> Map.replace_lazy(ie, :activities, &Enum.sort/1) end)
+    |> Enum.sort()
+  end
+
+  describe "pre_insert_hook/1" do
+    test "adds informed entities for parent stations" do
+      State.Stop.new_state([
+        %Model.Stop{id: "child-stop1", parent_station: "parent-stationA"},
+        %Model.Stop{id: "child-stop2", parent_station: "parent-stationB"},
+        %Model.Stop{id: "child-stop3", parent_station: "parent-stationB"},
+        %Model.Stop{id: "parentless-stop"}
+      ])
+
+      informed_entities =
+        [
+          %{stop: "child-stop1"},
+          %{stop: "child-stop2"},
+          %{stop: "child-stop3"},
+          %{stop: "parentless-stop"}
+        ]
+        |> normalize()
+
+      assert %{
+               preserved: ^informed_entities,
+               added: [%{stop: "parent-stationA"}, %{stop: "parent-stationB"}]
+             } = apply_hook(informed_entities)
+    end
+
+    test "merges child informed entities' activities for parent station informed entities," <>
+           " and does *not* merge activities for pre-existing child stop informed entities" do
+      State.Stop.new_state([
+        %Model.Stop{id: "child-stop1", parent_station: "parent-stationA"},
+        %Model.Stop{id: "child-stop2", parent_station: "parent-stationA"},
+        %Model.Stop{id: "child-stop3", parent_station: "parent-stationB"},
+        %Model.Stop{id: "child-stop4", parent_station: "parent-stationB"},
+        %Model.Stop{id: "child-stop5", parent_station: "parent-stationC"},
+        %Model.Stop{id: "parentless-stop"}
+      ])
+
+      informed_entities =
+        [
+          %{
+            stop: "child-stop1",
+            route: "route1",
+            activities: ["BOARD", "EXIT", "USING_WHEELCHAIR"]
+          },
+          %{stop: "child-stop1", route: "route2", activities: ["BOARD"]},
+          %{stop: "child-stop2", route: "route1", activities: ["BOARD", "EXIT", "RIDE"]},
+          %{stop: "child-stop3", activities: ["BOARD"], route: "route1", trip: "trip1"},
+          %{stop: "child-stop4", activities: ["EXIT"], route: "route1", trip: "trip1"},
+          %{stop: "child-stop4", activities: ["RIDE"], route: "route1", trip: "trip2"},
+          %{stop: "child-stop4", activities: ["RIDE"], route: "route2", trip: "trip1"},
+          %{stop: "child-stop5", activities: ["USING_ESCALATOR"]},
+          %{stop: "parentless-stop", activities: ["BRINGING_BIKE", "STORE_BIKE"]}
+        ]
+        |> normalize()
+
+      assert %{
+               preserved: ^informed_entities,
+               added: [
+                 %{stop: "parent-stationC", activities: ["USING_ESCALATOR"]},
+                 %{stop: "parent-stationA", route: "route2", activities: ["BOARD"]},
+                 %{
+                   stop: "parent-stationA",
+                   route: "route1",
+                   activities: ["BOARD", "EXIT", "RIDE", "USING_WHEELCHAIR"]
+                 },
+                 %{
+                   stop: "parent-stationB",
+                   activities: ["BOARD", "EXIT"],
+                   route: "route1",
+                   trip: "trip1"
+                 },
+                 %{stop: "parent-stationB", activities: ["RIDE"], route: "route1", trip: "trip2"},
+                 %{stop: "parent-stationB", activities: ["RIDE"], route: "route2", trip: "trip1"}
+               ]
+             } = apply_hook(informed_entities)
+    end
+
+    test "adds informed entities for alternate trips" do
+      State.Trip.new_state([
+        %Model.Trip{id: "trip1", alternate_route: false, route_id: "main-route"},
+        %Model.Trip{id: "trip1", alternate_route: true, route_id: "alt-route1"},
+        %Model.Trip{id: "trip1", alternate_route: true, route_id: "alt-route2"},
+        %Model.Trip{id: "trip1", alternate_route: true, route_id: "alt-routeX", direction_id: 1}
+      ])
+
+      State.Route.new_state([
+        %Model.Route{id: "main-route", type: 3},
+        %Model.Route{id: "alt-route1", type: 3},
+        %Model.Route{id: "alt-route2", type: 3}
+      ])
+
+      informed_entities =
+        [
+          %{
+            stop: "bus-stop1",
+            trip: "trip1",
+            route: "main-route",
+            direction_id: 0,
+            activities: ["BOARD", "EXIT"]
+          },
+          %{
+            stop: "bus-stop2",
+            trip: "trip1",
+            route: "a-different-route",
+            direction_id: 0,
+            activities: ["BOARD"]
+          },
+          %{stop: "bus-stop3", trip: "trip2", direction_id: 1}
+        ]
+        |> normalize()
+
+      assert %{
+               preserved: [
+                 %{stop: "bus-stop3", trip: "trip2", direction_id: 1},
+                 %{
+                   stop: "bus-stop2",
+                   trip: "trip1",
+                   route: "a-different-route",
+                   direction_id: 0,
+                   activities: ["BOARD"]
+                 }
+               ],
+               added: [
+                 %{
+                   stop: "bus-stop2",
+                   trip: "trip1",
+                   route: "alt-routeX",
+                   direction_id: 1,
+                   activities: ["BOARD"]
+                 },
+                 %{
+                   stop: "bus-stop1",
+                   trip: "trip1",
+                   route: "alt-routeX",
+                   direction_id: 1,
+                   activities: ["BOARD", "EXIT"]
+                 },
+                 %{
+                   stop: "bus-stop2",
+                   route: "alt-route1",
+                   direction_id: nil,
+                   route_type: 3,
+                   trip: "trip1",
+                   activities: ["BOARD"]
+                 },
+                 %{
+                   stop: "bus-stop2",
+                   route: "alt-route2",
+                   direction_id: nil,
+                   route_type: 3,
+                   trip: "trip1",
+                   activities: ["BOARD"]
+                 },
+                 %{
+                   stop: "bus-stop2",
+                   route: "main-route",
+                   direction_id: nil,
+                   route_type: 3,
+                   trip: "trip1",
+                   activities: ["BOARD"]
+                 },
+                 %{
+                   stop: "bus-stop1",
+                   route: "alt-route1",
+                   direction_id: nil,
+                   route_type: 3,
+                   trip: "trip1",
+                   activities: ["BOARD", "EXIT"]
+                 },
+                 %{
+                   stop: "bus-stop1",
+                   route: "alt-route2",
+                   direction_id: nil,
+                   route_type: 3,
+                   trip: "trip1",
+                   activities: ["BOARD", "EXIT"]
+                 },
+                 %{
+                   stop: "bus-stop1",
+                   route: "main-route",
+                   direction_id: nil,
+                   route_type: 3,
+                   trip: "trip1",
+                   activities: ["BOARD", "EXIT"]
+                 }
+               ],
+               removed: [
+                 %{
+                   stop: "bus-stop1",
+                   trip: "trip1",
+                   route: "main-route",
+                   direction_id: 0,
+                   activities: ["BOARD", "EXIT"]
+                 }
+               ]
+             } = apply_hook(informed_entities)
+    end
+
+    test "adds Cartesian product of computed informed entities for parent stations X alternate trips" do
+      State.Stop.new_state([
+        %Model.Stop{id: "busway-berth1", parent_station: "parent-stationA"},
+        %Model.Stop{id: "busway-berth2", parent_station: "parent-stationA"}
+      ])
+
+      State.Trip.new_state([
+        %Model.Trip{id: "trip1", alternate_route: false, route_id: "main-route"},
+        %Model.Trip{id: "trip1", alternate_route: true, route_id: "alt-route"}
+      ])
+
+      State.Route.new_state([
+        %Model.Route{id: "main-route", type: 3},
+        %Model.Route{id: "alt-route", type: 3}
+      ])
+
+      informed_entities =
+        [
+          %{
+            stop: "busway-berth1",
+            trip: "trip1",
+            route: "main-route",
+            direction_id: 0,
+            activities: ["BOARD", "EXIT"]
+          },
+          %{
+            stop: "busway-berth2",
+            trip: "trip1",
+            route: "main-route",
+            direction_id: 0,
+            activities: ["BOARD", "USING_WHEELCHAIR"]
+          }
+        ]
+        |> normalize()
+
+      assert %{
+               preserved: [],
+               added: [
+                 %{
+                   stop: "busway-berth1",
+                   route: "alt-route",
+                   direction_id: nil,
+                   route_type: 3,
+                   trip: "trip1",
+                   activities: ["BOARD", "EXIT"]
+                 },
+                 %{
+                   stop: "busway-berth1",
+                   route: "main-route",
+                   direction_id: nil,
+                   route_type: 3,
+                   trip: "trip1",
+                   activities: ["BOARD", "EXIT"]
+                 },
+                 %{
+                   stop: "parent-stationA",
+                   route: "alt-route",
+                   direction_id: nil,
+                   route_type: 3,
+                   trip: "trip1",
+                   activities: ["BOARD", "EXIT", "USING_WHEELCHAIR"]
+                 },
+                 %{
+                   stop: "parent-stationA",
+                   route: "main-route",
+                   direction_id: nil,
+                   route_type: 3,
+                   trip: "trip1",
+                   activities: ["BOARD", "EXIT", "USING_WHEELCHAIR"]
+                 },
+                 %{
+                   stop: "busway-berth2",
+                   route: "alt-route",
+                   direction_id: nil,
+                   route_type: 3,
+                   trip: "trip1",
+                   activities: ["BOARD", "USING_WHEELCHAIR"]
+                 },
+                 %{
+                   stop: "busway-berth2",
+                   route: "main-route",
+                   direction_id: nil,
+                   route_type: 3,
+                   trip: "trip1",
+                   activities: ["BOARD", "USING_WHEELCHAIR"]
+                 }
+               ],
+               removed: [
+                 %{
+                   stop: "busway-berth1",
+                   route: "main-route",
+                   direction_id: 0,
+                   trip: "trip1",
+                   activities: ["BOARD", "EXIT"]
+                 },
+                 %{
+                   stop: "busway-berth2",
+                   route: "main-route",
+                   direction_id: 0,
+                   trip: "trip1",
+                   activities: ["BOARD", "USING_WHEELCHAIR"]
+                 }
+               ]
+             } = apply_hook(informed_entities)
+    end
+
+    test "handles specific case from bugfix ticket" do
+      # https://app.asana.com/1/15492006741476/project/584764604969369/task/1213450825693783?focus=true
+
+      State.Stop.new_state([
+        %Model.Stop{id: "BNT-0000", parent_station: "place-north"},
+        %Model.Stop{id: "WR-0045-S", parent_station: "place-mlmnl"},
+        %Model.Stop{id: "WR-0053-S", parent_station: "place-ogmnl"}
+      ])
+
+      assert [alert] = Parse.Alerts.parse(alerts_enhanced_json_excerpt())
+
+      informed_entities = normalize(alert.informed_entity)
+
+      expected_new_informed_entities =
+        [
+          %{
+            stop: "place-mlmnl",
+            activities: ["BOARD", "EXIT", "RIDE"],
+            route: "CR-Haverhill",
+            direction_id: 1,
+            route_type: 2
+          },
+          %{
+            stop: "place-mlmnl",
+            activities: ["BOARD", "EXIT", "RIDE"],
+            route: "CR-Haverhill",
+            direction_id: 0,
+            route_type: 2
+          },
+          %{
+            stop: "place-north",
+            activities: ["BOARD", "EXIT", "RIDE"],
+            route: "CR-Haverhill",
+            direction_id: 1,
+            route_type: 2
+          },
+          %{
+            stop: "place-north",
+            activities: ["BOARD", "EXIT", "RIDE"],
+            route: "CR-Haverhill",
+            direction_id: 0,
+            route_type: 2
+          },
+          %{
+            stop: "place-ogmnl",
+            activities: ["BOARD", "EXIT", "RIDE"],
+            route: "CR-Haverhill",
+            direction_id: 1,
+            route_type: 2
+          },
+          %{
+            stop: "place-ogmnl",
+            activities: ["BOARD", "EXIT", "RIDE"],
+            route: "CR-Haverhill",
+            direction_id: 0,
+            route_type: 2
+          }
+        ]
+        |> normalize()
+
+      assert %{
+               preserved: ^informed_entities,
+               added: ^expected_new_informed_entities
+             } = apply_hook(alert)
+    end
+  end
+
+  defp alerts_enhanced_json_excerpt do
+    ~S"""
+    {
+      "entity": [
+        {
+          "id": "1000217",
+          "alert": {
+            "cause": "UNKNOWN_CAUSE",
+            "effect": "REDUCED_SERVICE",
+            "severity": 7,
+            "active_period": [
+              {
+                "start": 1772415300,
+                "end": 1772697600
+              }
+            ],
+            "duration_certainty": "KNOWN",
+            "cause_detail": "UNKNOWN_CAUSE",
+            "effect_detail": "SUSPENSION",
+            "informed_entity": [
+              {
+                "direction_id": 1,
+                "stop_id": "BNT-0000",
+                "route_id": "CR-Haverhill",
+                "route_type": 2,
+                "agency_id": "1",
+                "activities": [
+                  "EXIT",
+                  "RIDE"
+                ]
+              },
+              {
+                "direction_id": 1,
+                "stop_id": "WR-0045-S",
+                "route_id": "CR-Haverhill",
+                "route_type": 2,
+                "agency_id": "1",
+                "activities": [
+                  "BOARD",
+                  "EXIT",
+                  "RIDE"
+                ]
+              },
+              {
+                "direction_id": 1,
+                "stop_id": "WR-0053-S",
+                "route_id": "CR-Haverhill",
+                "route_type": 2,
+                "agency_id": "1",
+                "activities": [
+                  "BOARD",
+                  "RIDE"
+                ]
+              },
+              {
+                "direction_id": 0,
+                "stop_id": "BNT-0000",
+                "route_id": "CR-Haverhill",
+                "route_type": 2,
+                "agency_id": "1",
+                "activities": [
+                  "BOARD",
+                  "RIDE"
+                ]
+              },
+              {
+                "direction_id": 0,
+                "stop_id": "WR-0045-S",
+                "route_id": "CR-Haverhill",
+                "route_type": 2,
+                "agency_id": "1",
+                "activities": [
+                  "BOARD",
+                  "EXIT",
+                  "RIDE"
+                ]
+              },
+              {
+                "direction_id": 0,
+                "stop_id": "WR-0053-S",
+                "route_id": "CR-Haverhill",
+                "route_type": 2,
+                "agency_id": "1",
+                "activities": [
+                  "EXIT",
+                  "RIDE"
+                ]
+              }
+            ],
+            "last_modified_timestamp": 1772415513,
+            "severity_level": "SEVERE",
+            "header_text": {
+              "translation": [
+                {
+                  "text": "Haverhill Line: service suspended between North Station and Oak Grove today.",
+                  "language": "en"
+                }
+              ]
+            },
+            "description_text": {
+              "translation": [
+                {
+                  "text": "Affected stations:\r\nNorth Station\r\nMalden Center\r\nOak Grove",
+                  "language": "en"
+                }
+              ]
+            },
+            "service_effect_text": {
+              "translation": [
+                {
+                  "text": "Suspension of service on Haverhill Line",
+                  "language": "en"
+                }
+              ]
+            },
+            "created_timestamp": 1772415356,
+            "alert_lifecycle": "NEW"
+          }
+        }
+      ]
+    }
+    """
   end
 end
