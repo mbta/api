a38f56d0d74b2cac179754a66ba5daf1f35af1ae
Corey N. Runkel <crunkel@mbta.com>
2026-03-23 09:24:25 -0400

    Improve test coverage

--- apps/api_web/test/api_web/views/stop_event_view_test.exs
+++ apps/api_web/test/api_web/views/stop_event_view_test.exs
@@ -86,13 +86,9 @@ defmodule ApiWeb.StopEventViewTest do
     assert rendered["data"]["id"] == "trip1-route1-v1-1"
 
     assert rendered["data"]["attributes"] == %{
-             "vehicle_id" => "v1",
              "start_date" => ~D[2026-02-24],
-             "trip_id" => "trip1",
              "direction_id" => 0,
-             "route_id" => "route1",
              "revenue" => :REVENUE,
-             "stop_id" => "stop1",
              "stop_sequence" => 1,
              "arrived" => "2026-02-24T15:28:06Z",
              "departed" => "2026-02-24T15:40:46Z"
@@ -139,7 +135,7 @@ defmodule ApiWeb.StopEventViewTest do
       :ok
     end
 
-    test "includes all default relationships", %{conn: conn} do
+    test "includes all default relationships but no optional relationships", %{conn: conn} do
       rendered =
         render(ApiWeb.StopEventView, "index.json-api",
           data: @stop_event,
@@ -191,7 +187,9 @@ defmodule ApiWeb.StopEventViewTest do
                "schedule-trip1-stop1-2"
     end
 
-    test "includes schedule relationship when requested", %{conn: conn} do
+    test "includes schedule relationship plus all default relationships when requested", %{
+      conn: conn
+    } do
       State.RoutePattern.new_state([@route_pattern])
       State.Schedule.new_state([@schedule])
       State.RoutesPatternsAtStop.update!()
@@ -206,8 +204,12 @@ defmodule ApiWeb.StopEventViewTest do
           conn: conn
         )
 
-      assert get_in(rendered, ["data", "relationships", "schedule", "data", "id"]) ==
-               "schedule-trip1-stop1-1"
+      relationships = rendered["data"]["relationships"]
+      assert relationships["trip"]["data"]["id"] == "trip1"
+      assert relationships["stop"]["data"]["id"] == "stop1"
+      assert relationships["route"]["data"]["id"] == "route1"
+      assert relationships["vehicle"]["data"]["id"] == "v1"
+      assert relationships["schedule"]["data"]["id"] == "schedule-trip1-stop1-1"
     end
 
     test "returns nil schedule when schedule does not exist", %{conn: conn} do
