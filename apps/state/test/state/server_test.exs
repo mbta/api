32c8422265b00b861d100ff512a57431b283a374
Emily Crandall Fleischman <ecfleischman@mbta.com>
2026-07-22 14:39:05 -0400

    chore: order-insensitive test

--- apps/state/test/state/server_test.exs
+++ apps/state/test/state/server_test.exs
@@ -251,7 +251,11 @@ defmodule State.ServerTest do
         %Example{id: 2, data: :other}
       ])
 
-      assert [%{id: 2, data: :other}, %{id: 1, data: 38}, %{id: 1, data: 44}] = HooksServer.all()
+      all = HooksServer.all()
+      assert length(all) == 3
+      assert %{id: 1, data: 38} in all
+      assert %{id: 1, data: 44} in all
+      assert %{id: 2, data: :other} in all
       assert [%{data: 38}, %{data: 44}] = HooksServer.by_id(1)
       assert [%{data: 38}] = HooksServer.select([%{data: 37}])
     end
